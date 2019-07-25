90716f62dedadb2eeec69f4ca09ded9393b42d88
jennifer <jennifer.malleret@outlook.fr>
2019-07-25 11:40:25 +0200

    exercice 5 php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>Exercice 5</title>
+</head>
+<body>
+  <h1>Créer un tableau avec les mois de l'année</h1>
+    <?php
+      $monthsList = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre','Novembre', 'Décembre'];
+      print_r($monthsList);
+    ?>
+    </body>
+    </html>
